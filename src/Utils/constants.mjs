3d009902bb8ff22a2f6e93f9582f854f20b5d9bf
John Muchiri <muchirijohn0018@gmail.com>
2025-10-21 15:05:43 +0300

    sessions pt2 and authentication

--- src/Utils/constants.mjs
+++ src/Utils/constants.mjs
@@ -1,12 +1,12 @@
 export const mockUsers = ([
-  { id: 1, userName: "John", displayName: "Jonte" },
-  { id: 2, userName: "James", displayName: "Makomele" },
-  { id: 3, userName: "Mary", displayName: "Mrs kiazala" },
-  { id: 4, userName: "Leah", displayName: "kafupi" },
-  { id: 5, userName: "Mercy", displayName: "kanono" },
-  { id: 6, userName: "Muthoni", displayName: "ras baby" },
-  { id: 7, userName: "kim", displayName: "magenta" },
-  { id: 8, userName: "Steve", displayName: "kip" }
+  { id: 1, userName: "John", displayName: "Jonte", password: "hello123" },
+  { id: 2, userName: "James", displayName: "Makomele", password: "hello124" },
+  { id: 3, userName: "Mary", displayName: "Mrs kiazala", password: "hello224" },
+  { id: 4, userName: "Leah", displayName: "kafupi", password: "hello323" },
+  { id: 5, userName: "Mercy", displayName: "kanono" , password: "hello423"},
+  { id: 6, userName: "Muthoni", displayName: "ras baby", password: "hello321" },
+  { id: 7, userName: "kim", displayName: "magenta" , password: "hello234"},
+  { id: 8, userName: "Steve", displayName: "kip", password: "hello332" }
 ])
 
 
--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,7 @@ import express, { request, response } from 'express';
 import routes from './Routes/index.mjs' //file with all the routers
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { mockUsers } from './Utils/constants.mjs';
 
 
 const app = express();
@@ -28,15 +29,55 @@ app.get('/', (request, response) => {
 
 });
 
-app.use(routes);// activate the file with all the routers
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
 
+app.post('/api/auth', (request, response) =>{
+  const {
+    //destructure to grab username and password
+    body: {userName, password}
+  } = request;
+  console.log('BODY:', request.body);
+  const findUser = mockUsers.find((user) => user.userName === userName);
+  if (!findUser || findUser.password !== password)
+    return response.status(401).send('BAD CREDENTIALS');
+  //below stores the user object in the session
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
+ app.get('/api/auth/status', (request, response) => {
+  request.sessionStore.get(request.sessionID, (err, session) =>{
+    console.log(session);
+  })
+  return request.session.user ? response.status(200).send(request.session.user) : response.status(401).send ({msg: 'Not authenticated'})
+ })
+
+app.post('/api/cart', (request, response) =>{
+  if(!request.session.user) return response.sendStatus(401)
+    const item= request.body;
+
+  const {cart} = request.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    request.session.cart = [item];
+  }
+  return response.status(201).send(item);
+});
 
 
+app.get('/api/cart', (request, response) =>{
+  //if there's no user defined in the session object, send that status
+   if(!request.session.user) return response.sendStatus(401)
+    //send the session cat but if undefined, return an empty array
+    return response.send(request.session.cart ?? [])
+})
+
+app.use(routes);// activate the file with all the routers
 
 
 
