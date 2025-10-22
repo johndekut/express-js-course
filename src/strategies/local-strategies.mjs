dc1362f9b95ec34cc074bc4cb8ea8bfd05ac86f6
John Muchiri <muchirijohn0018@gmail.com>
2025-10-22 19:01:04 +0300

    replacing mockUsers with actual data in the database

--- src/strategies/local-strategies.mjs
+++ src/strategies/local-strategies.mjs
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
 import { mockUsers } from "../Utils/constants.mjs";
+import { user } from "../mongoose/schemas/userSchema.mjs";
 
 //below isonly called once
 passport.serializeUser((user, done) =>{
@@ -12,11 +13,11 @@ passport.serializeUser((user, done) =>{
 });
 //deserealize is used to unpack the user object using the key
 //userName/id --unique is what we use to get other data regarding the user
-passport.deserializeUser((id, done) =>{
+passport.deserializeUser(async (id, done) =>{
   console.log(`inside deserialze`);
   console.log(`Deserializing user Id: ${id}`)
 try{
-const findUser = mockUsers.find((user) => user.id === id);
+const findUser = await user.findById(id)
 if(!findUser) throw new Error("User not found")
   //if user is found, return the user, set error to null
 done(null, findUser);
@@ -26,20 +27,18 @@ done(null, findUser);
 }
 })
 export default passport.use(
-  new Strategy(
+  new Strategy( 
     {
       usernameField:"userName",
       passwordField:"password",
     },
     
-    (userName, password, done) => {
-    console.log(`user name: ${userName}`);
-    console.log(`Password: ${password}`);
-
-
+    async (userName, password, done) => {
     try {
-
-     
+      const findUser= await user.findOne({userName})
+     if (!findUser) throw new Error("User not Found");
+     if (findUser.password !== password) throw new Error("Bad credentials");
+     done(null, findUser)
     } catch (err) {
       //err- hold info on what went wrong
       done(err, null);
