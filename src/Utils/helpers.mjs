7f6d5e65a7d2fb6872d7f7489e6bd44b75027213
John Muchiri <muchirijohn0018@gmail.com>
2025-10-22 20:02:22 +0300

    hashing passwords instead of using raw passwords visible to anyone in the db

--- src/Utils/helpers.mjs
+++ src/Utils/helpers.mjs
@@ -3,5 +3,9 @@ import bcrypt from  'bcrypt';
 //saltRounds control how much randomness is added when hashing passwords: the more the ~ te stronger the password
 const saltRounds = 10;
 export const hashPassword = (password) =>{
-
+  //generate the salt
+  const salt =bcrypt.genSaltSync(saltRounds)
+  console.log(salt)
+  return bcrypt.hashSync(password, salt);
+  
 }
