bac9cc87fd84690783d1403ed5c53a0deead4006
John Muchiri <muchirijohn0018@gmail.com>
2025-10-22 19:16:40 +0300

    install bcrypt for hashing passwords and create a separate file for its logic

---
+++ src/Utils/helpers.mjs
@@ -0,0 +1,7 @@
+import bcrypt from  'bcrypt';
+
+//saltRounds control how much randomness is added when hashing passwords: the more the ~ te stronger the password
+const saltRounds = 10;
+export const hashPassword = (password) =>{
+
+}
