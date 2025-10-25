2e2b01b6d6c246d06a92a234c7747e315b95ea77
John Muchiri <muchirijohn0018@gmail.com>
2025-10-25 21:40:27 +0300

    first succcesful test and correted the babel configuration

--- src/__test__/users.test.js
+++ src/__test__/users.test.js
@@ -2,13 +2,20 @@ import { getUserByIdHandler } from "../handlers/users.mjs";
 
 
 const mockRequest = {
-  
+  //request in the original function sends the userindex
+ params:{
+  id: 1
+ }};
+const mockResponse =  {
+  //response in the main function sends back a sttatus
+  //create a mock function for that as below not the actual funtion
+  sendStatus: jest.fn(),
+  send:jest.fn()
 };
-const mockResponse =  {};
 
 describe('get users', () =>{
   it ('should get user by id',() =>{
-    getUserByIdHandler()
+    getUserByIdHandler(mockRequest, mockResponse);
   })
 
 })
