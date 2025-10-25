c009057e0c464e7d4e4cc7cc572ad1eaff9c5821
John Muchiri <muchirijohn0018@gmail.com>
2025-10-25 23:20:59 +0300

    iplemented a few ssertion tests

--- src/__test__/users.test.js
+++ src/__test__/users.test.js
@@ -1,4 +1,6 @@
+import {mockUsers} from '../Utils/constants.mjs';
 import { getUserByIdHandler } from "../handlers/users.mjs";
+
 
 
 const mockRequest = {
@@ -16,6 +18,19 @@ const mockResponse =  {
 describe('get users', () =>{
   it ('should get user by id',() =>{
     getUserByIdHandler(mockRequest, mockResponse);
-  })
+    expect(mockResponse.send).toHaveBeenCalled();
+    expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[0]);
+    expect(mockResponse.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call sendStatus of 404 when user is not found', () =>{
+    const copyMockRequest = {...mockRequest,params:{id:100} }
+    getUserByIdHandler(copyMockRequest, mockResponse);
+    //below are called assertions
+    expect(mockResponse.sendStatus).toHaveBeenCalled();
+    expect(mockResponse.sendStatus).toHaveBeenCalledTimes(1);
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(400);
+    expect(mockResponse.send).not.toHaveBeenCalled();
+  });
 
 })
