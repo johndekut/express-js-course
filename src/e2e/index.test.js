89c2d2608afd9deeddd97c3fb6d0adb47fe546cc
John Muchiri <muchirijohn0018@gmail.com>
2025-10-27 16:34:34 +0300

    fix: correct test:e2e script and add initial hello endpoint test

--- src/e2e/index.test.js
+++ src/e2e/index.test.js
@@ -1,1 +1,16 @@
-import supertest from "supertest";
+import request from "supertest";
+import express, { response } from 'express';
+
+const app = express();//set an express app
+
+app.get('/hello', (req, res) => {
+  res.status(200).send({});
+});
+
+describe('hello endpoint', () => {
+  it('get /hello and expect 200 status code', async () => {
+    const response = await request(app).get('/hello');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({});
+  });
+});
